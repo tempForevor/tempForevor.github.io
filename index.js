28e5c628898a8255f47ac4d5582db91f5e89d638
tempForevor <e-mjcsmail@qq.com>
2024-04-05 12:16:17 +0800

    fixed some bugs // index.js -> fonload.add(OnLoad()) changed to fonload.add(OnLoad)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var global_timeSetter = 0
 function timeSetter(){
+	dom.$("#TimeSetter").innerText = new Date().toString()
 	global_timeSetter = setInterval( ()=>{
 		dom.$("#TimeSetter").innerText = new Date().toString()
 	},1000)
@@ -8,4 +9,4 @@ function OnLoad(){
 	timeSetter()
 }
 
-fonload.add(OnLoad())
+fonload.add(OnLoad)
--- nav.js
+++ nav.js
@@ -1,6 +1,7 @@
 fonload.add(()=>{
     var navblock = document.getElementsByClassName("nav-block")
-    const add = (link,str)=>{
+    const add = (link,str,special="")=>{
+        if(!RegExp(special).test(file_name))return
         if(str==file_name) navblock[0].innerHTML +=
                 "\n<li class=\"nav disabled\"><span>"+str+"</a></li>"
         else navblock[0].innerHTML +=
@@ -8,4 +9,5 @@ fonload.add(()=>{
     }
     add("https://tempforevor.github.io/","Main page")
     add("https://tempforevor.github.io/Timer/Timer.htm","Timer")
+    add("#about","about","Main page")
 })
